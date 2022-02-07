4abea347a50fd4a4b9ec10cd1e0f68bd3574c395
nirzaf <mfmfazrin1986@gmail.com>
2022-02-07 20:52:42 +0530

    Add project files.

---
+++ setting_form/Form1.Designer.cs
@@ -0,0 +1,425 @@
+﻿namespace setting_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lang_comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.add_textBox10 = new System.Windows.Forms.TextBox();
+            this.site_textBox9 = new System.Windows.Forms.TextBox();
+            this.email_textBox8 = new System.Windows.Forms.TextBox();
+            this.act_textBox7 = new System.Windows.Forms.TextBox();
+            this.fax_textBox6 = new System.Windows.Forms.TextBox();
+            this.cell_textBox5 = new System.Windows.Forms.TextBox();
+            this.tel_textBox4 = new System.Windows.Forms.TextBox();
+            this.adv_textBox3 = new System.Windows.Forms.TextBox();
+            this.manager_textBox2 = new System.Windows.Forms.TextBox();
+            this.title_textBox1 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(391, 411);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label11);
+            this.tabPage1.Controls.Add(this.lang_comboBox1);
+            this.tabPage1.Controls.Add(this.label10);
+            this.tabPage1.Controls.Add(this.label9);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.add_textBox10);
+            this.tabPage1.Controls.Add(this.site_textBox9);
+            this.tabPage1.Controls.Add(this.email_textBox8);
+            this.tabPage1.Controls.Add(this.act_textBox7);
+            this.tabPage1.Controls.Add(this.fax_textBox6);
+            this.tabPage1.Controls.Add(this.cell_textBox5);
+            this.tabPage1.Controls.Add(this.tel_textBox4);
+            this.tabPage1.Controls.Add(this.adv_textBox3);
+            this.tabPage1.Controls.Add(this.manager_textBox2);
+            this.tabPage1.Controls.Add(this.title_textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(383, 385);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Application Settings";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 31);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Save Changes F5";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(25, 292);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(58, 13);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Language:";
+            // 
+            // lang_comboBox1
+            // 
+            this.lang_comboBox1.FormattingEnabled = true;
+            this.lang_comboBox1.Items.AddRange(new object[] {
+            "English",
+            "French"});
+            this.lang_comboBox1.Location = new System.Drawing.Point(96, 289);
+            this.lang_comboBox1.Name = "lang_comboBox1";
+            this.lang_comboBox1.Size = new System.Drawing.Size(268, 21);
+            this.lang_comboBox1.TabIndex = 20;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(25, 255);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(48, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Address:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(25, 229);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(54, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Web Site:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(25, 203);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Email:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 177);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Activity:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Fax:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Mobile:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Tel:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Advertising:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Manager:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Title:";
+            // 
+            // add_textBox10
+            // 
+            this.add_textBox10.Location = new System.Drawing.Point(96, 252);
+            this.add_textBox10.Name = "add_textBox10";
+            this.add_textBox10.Size = new System.Drawing.Size(268, 20);
+            this.add_textBox10.TabIndex = 9;
+            // 
+            // site_textBox9
+            // 
+            this.site_textBox9.Location = new System.Drawing.Point(96, 226);
+            this.site_textBox9.Name = "site_textBox9";
+            this.site_textBox9.Size = new System.Drawing.Size(268, 20);
+            this.site_textBox9.TabIndex = 8;
+            // 
+            // email_textBox8
+            // 
+            this.email_textBox8.Location = new System.Drawing.Point(96, 200);
+            this.email_textBox8.Name = "email_textBox8";
+            this.email_textBox8.Size = new System.Drawing.Size(268, 20);
+            this.email_textBox8.TabIndex = 7;
+            // 
+            // act_textBox7
+            // 
+            this.act_textBox7.Location = new System.Drawing.Point(96, 174);
+            this.act_textBox7.Name = "act_textBox7";
+            this.act_textBox7.Size = new System.Drawing.Size(268, 20);
+            this.act_textBox7.TabIndex = 6;
+            // 
+            // fax_textBox6
+            // 
+            this.fax_textBox6.Location = new System.Drawing.Point(96, 148);
+            this.fax_textBox6.Name = "fax_textBox6";
+            this.fax_textBox6.Size = new System.Drawing.Size(268, 20);
+            this.fax_textBox6.TabIndex = 5;
+            // 
+            // cell_textBox5
+            // 
+            this.cell_textBox5.Location = new System.Drawing.Point(96, 122);
+            this.cell_textBox5.Name = "cell_textBox5";
+            this.cell_textBox5.Size = new System.Drawing.Size(268, 20);
+            this.cell_textBox5.TabIndex = 4;
+            this.cell_textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // tel_textBox4
+            // 
+            this.tel_textBox4.Location = new System.Drawing.Point(96, 96);
+            this.tel_textBox4.Name = "tel_textBox4";
+            this.tel_textBox4.Size = new System.Drawing.Size(268, 20);
+            this.tel_textBox4.TabIndex = 3;
+            // 
+            // adv_textBox3
+            // 
+            this.adv_textBox3.Location = new System.Drawing.Point(96, 70);
+            this.adv_textBox3.Name = "adv_textBox3";
+            this.adv_textBox3.Size = new System.Drawing.Size(268, 20);
+            this.adv_textBox3.TabIndex = 2;
+            // 
+            // manager_textBox2
+            // 
+            this.manager_textBox2.Location = new System.Drawing.Point(96, 44);
+            this.manager_textBox2.Name = "manager_textBox2";
+            this.manager_textBox2.Size = new System.Drawing.Size(268, 20);
+            this.manager_textBox2.TabIndex = 1;
+            // 
+            // title_textBox1
+            // 
+            this.title_textBox1.Location = new System.Drawing.Point(96, 18);
+            this.title_textBox1.Name = "title_textBox1";
+            this.title_textBox1.Size = new System.Drawing.Size(268, 20);
+            this.title_textBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.panel1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(383, 385);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Logo";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(14, 9);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(352, 365);
+            this.panel1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(123, 263);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 33);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Save Logo";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(123, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Select Image";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(104, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(170, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(18, 332);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(55, 27);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Load";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 435);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Setting";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox cell_textBox5;
+        private System.Windows.Forms.TextBox tel_textBox4;
+        private System.Windows.Forms.TextBox adv_textBox3;
+        private System.Windows.Forms.TextBox manager_textBox2;
+        private System.Windows.Forms.TextBox title_textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox lang_comboBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox add_textBox10;
+        private System.Windows.Forms.TextBox site_textBox9;
+        private System.Windows.Forms.TextBox email_textBox8;
+        private System.Windows.Forms.TextBox act_textBox7;
+        private System.Windows.Forms.TextBox fax_textBox6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ setting_form/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace setting_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //////this.title_textBox1.Text = setting_form.Properties.Settings.Default.co_title;
+            //////this.manager_textBox2.Text = setting_form.Properties.Settings.Default.co_manager;
+            //////this.adv_textBox3.Text = setting_form.Properties.Settings.Default.co_adv;
+            //////this.tel_textBox4.Text = setting_form.Properties.Settings.Default.co_tel;
+            //////this.cell_textBox5.Text = setting_form.Properties.Settings.Default.co_cell;
+            //////this.fax_textBox6 .Text = setting_form.Properties.Settings.Default.co_fax ;
+            //////this.act_textBox7.Text = setting_form.Properties.Settings.Default.co_activity ;
+            //////this.email_textBox8 .Text = setting_form.Properties.Settings.Default.co_email ;
+            //////this.site_textBox9.Text = setting_form.Properties.Settings.Default.co_site ;
+            //////this.add_textBox10.Text = setting_form.Properties.Settings.Default.co_add ;
+            //////this.lang_comboBox1 .Text = setting_form.Properties.Settings.Default.co_language;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // button4_Click(sender, e);
+            this.title_textBox1.Text = setting_form.Properties.Settings.Default.co_title;
+            this.manager_textBox2.Text = setting_form.Properties.Settings.Default.co_manager;
+            this.adv_textBox3.Text = setting_form.Properties.Settings.Default.co_adv;
+            this.tel_textBox4.Text = setting_form.Properties.Settings.Default.co_tel;
+            this.cell_textBox5.Text = setting_form.Properties.Settings.Default.co_cell;
+            this.fax_textBox6.Text = setting_form.Properties.Settings.Default.co_fax;
+            this.act_textBox7.Text = setting_form.Properties.Settings.Default.co_activity;
+            this.email_textBox8.Text = setting_form.Properties.Settings.Default.co_email;
+            this.site_textBox9.Text = setting_form.Properties.Settings.Default.co_site;
+            this.add_textBox10.Text = setting_form.Properties.Settings.Default.co_add;
+            this.lang_comboBox1.Text = setting_form.Properties.Settings.Default.co_language;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            setting_form.Properties.Settings.Default.co_title = this.title_textBox1.Text;
+            setting_form.Properties.Settings.Default.co_manager = this.manager_textBox2.Text;
+            setting_form.Properties.Settings.Default.co_adv = this.adv_textBox3.Text;
+            setting_form.Properties.Settings.Default.co_tel = this.tel_textBox4.Text;
+            setting_form.Properties.Settings.Default.co_cell = this.cell_textBox5.Text;
+            setting_form.Properties.Settings.Default.co_fax = this.fax_textBox6.Text;
+            setting_form.Properties.Settings.Default.co_activity = this.act_textBox7.Text;
+            setting_form.Properties.Settings.Default.co_email = this.email_textBox8.Text;
+            setting_form.Properties.Settings.Default.co_site = this.site_textBox9.Text;
+            setting_form.Properties.Settings.Default.co_add = this.add_textBox10.Text;
+            setting_form.Properties.Settings.Default.co_language = this.lang_comboBox1.Text;
+            setting_form.Properties.Settings.Default.Save();
+            MessageBox.Show("Settings Saved!");
+        }
+    }
+}
